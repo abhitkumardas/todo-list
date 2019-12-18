55caae65a118568f6e4e60ae6c7b6d1eadca40d2
feroshkhan <ferozk@obsessory.com>
2019-12-19 00:20:13 +0530

    added new model table

---
+++ src/main/java/com/adtech/todolist/model/User.java
@@ -0,0 +1,62 @@
+package com.adtech.todolist.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "todo_users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USER_SEQ")
+    @SequenceGenerator(name = "USER_SEQ", sequenceName = "USER_SEQ")
+    @Column(name = "user_id", nullable = false,updatable = false)
+    private Long userId;
+    @Column(name="user_name")
+    private String userName;
+    @Column(name="guid")
+    private String guid;
+    @Column(name="create_date")
+    private Date createDate;
+    @Column(name="email")
+    private String email;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
